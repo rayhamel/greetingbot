6fda51915e65c46e97cbd393219b6e1397f2a4b3
Ray Hamel <rayghamel@gmail.com>
2017-02-01 12:56:17 -0500

    Change message on repeat introductions

--- greeting/introduce.js
+++ greeting/introduce.js
@@ -6,13 +6,15 @@ const greeterMsg = require("../helpers/greeterMsg");
 
 module.exports.introduce = (event, context, callback) => {
     const name = event.pathParameters.name;
-    const params = {Item: {greeting: greet(name), name}, TableName: "friends"};
+    const params = {Item: {greeting: greet(name), name},
+                    ReturnValues: "ALL_OLD", TableName: "friends"};
     db.put(params, (err, data) => {
         if (err) {
             callback(err);
             return;
         }
-        callback(null, {body: greeterMsg(`Nice to meet you, ${name}!`),
+        const welcome = data.Attributes ? 'We meet again' : 'Nice to meet you';
+        callback(null, {body: greeterMsg(`${welcome}, ${name}!`),
                         statusCode: 200});
     });
 };
